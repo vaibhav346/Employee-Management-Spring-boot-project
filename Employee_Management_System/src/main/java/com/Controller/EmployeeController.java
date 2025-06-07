427de0e9e1e6113202f5216750c1ca5fc2a6a8c7
vaibhav <vaibhavdhere6@gmail.com>
2025-06-07 17:55:00 +0530

    update code sucessfully run

--- Employee_Management_System/src/main/java/com/Controller/EmployeeController.java
+++ Employee_Management_System/src/main/java/com/Controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Entity.Employee;
 import com.Service.EmployeeService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
